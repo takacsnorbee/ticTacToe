14f086713c8980c90654bc3c3427178b0b7764cf
takacsnorbee <38784102+takacsnorbee@users.noreply.github.com>
2019-03-07 08:08:19 +0100

    Ready to play

---
+++ code.js
@@ -0,0 +1,146 @@
+//DATE
+document.getElementById('copyrightYear').innerHTML = new Date().getFullYear()
+
+//GAME---------------------------------------------------------------------
+
+//FIELDS
+
+let field1 = document.getElementById('field1'),
+		field2 = document.getElementById('field2'),
+		field3 = document.getElementById('field3'),
+		field4 = document.getElementById('field4'),
+		field5 = document.getElementById('field5'),
+		field6 = document.getElementById('field6'),
+		field7 = document.getElementById('field7'),
+		field8 = document.getElementById('field8'),
+		field9 = document.getElementById('field9')
+
+let fieldArray = [field1, field2, field3, field4, field5, field6, field7, field8, field9]
+
+//START GAME
+
+function resetGame() {
+	
+	for(let i in fieldArray) {
+		fieldArray[i].innerHTML = ' '
+		fieldArray[i].style.backgroundColor = 'white'
+	}
+	document.getElementById('result').innerHTML = ''
+	hasWinner = false
+}
+
+//MAKE MOVE
+	
+let player1 = true
+let player2 = false
+let hasWinner = false
+
+for(let j in fieldArray) {
+
+	fieldArray[j].onclick = ()=> {
+		if(fieldArray[j].innerHTML !== 'x' && fieldArray[j].innerHTML !== 'o' && hasWinner === false) {
+			if(player1 === true) {
+				fieldArray[j].innerHTML = 'x'
+				player1 = false
+				player2 = true
+				checkWinner()
+			} else {
+				fieldArray[j].innerHTML = 'o'
+				player1 = true
+				player2 = false
+				checkWinner()
+			}
+		}
+	}
+}
+
+//CHECK WINNER	
+
+function checkWinner() {
+		
+	if(fieldArray[0].innerHTML !== ' ' && fieldArray[1].innerHTML === 'x' && fieldArray[0].innerHTML === fieldArray[1].innerHTML && fieldArray[0].innerHTML === fieldArray[2].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[0], fieldArray[1], fieldArray[2])
+		hasWinner = true
+	} else if(fieldArray[3].innerHTML !== ' ' && fieldArray[3].innerHTML === 'x' && fieldArray[3].innerHTML === fieldArray[4].innerHTML && fieldArray[3].innerHTML === fieldArray[5].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[3], fieldArray[4], fieldArray[5])
+		hasWinner = true
+	} else if(fieldArray[6].innerHTML !== ' ' && fieldArray[6].innerHTML === 'x' && fieldArray[6].innerHTML === fieldArray[7].innerHTML && fieldArray[6].innerHTML === fieldArray[8].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[6], fieldArray[7], fieldArray[8])
+		hasWinner = true
+	} else if(fieldArray[0].innerHTML !== ' ' && fieldArray[0].innerHTML === 'x' && fieldArray[0].innerHTML === fieldArray[3].innerHTML && fieldArray[0].innerHTML === fieldArray[6].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[0], fieldArray[3], fieldArray[6])
+		hasWinner = true
+	} else if(fieldArray[1].innerHTML !== ' ' && fieldArray[1].innerHTML === 'x' && fieldArray[1].innerHTML === fieldArray[4].innerHTML && fieldArray[1].innerHTML === fieldArray[7].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[1], fieldArray[4], fieldArray[7])
+		hasWinner = true
+	} else if(fieldArray[2].innerHTML !== ' ' && fieldArray[2].innerHTML === 'x' && fieldArray[2].innerHTML === fieldArray[5].innerHTML && fieldArray[2].innerHTML === fieldArray[8].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[2], fieldArray[5], fieldArray[8])
+		hasWinner = true
+	} else if(fieldArray[0].innerHTML !== ' ' && fieldArray[0].innerHTML === 'x' && fieldArray[0].innerHTML === fieldArray[4].innerHTML && fieldArray[0].innerHTML === fieldArray[8].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[0], fieldArray[4], fieldArray[8])
+		hasWinner = true
+	} else if(fieldArray[2].innerHTML !== ' ' && fieldArray[2].innerHTML === 'x' && fieldArray[2].innerHTML === fieldArray[4].innerHTML && fieldArray[2].innerHTML === fieldArray[6].innerHTML) {
+		document.getElementById('result').innerHTML = 'X win!'
+		colorOfWinner(fieldArray[2], fieldArray[4], fieldArray[6])
+		hasWinner = true
+	} else if(fieldArray[0].innerHTML !== ' ' && fieldArray[1].innerHTML === 'o' && fieldArray[0].innerHTML === fieldArray[1].innerHTML && fieldArray[0].innerHTML === fieldArray[2].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[0], fieldArray[1], fieldArray[2])
+		hasWinner = true
+	} else if(fieldArray[3].innerHTML !== ' ' && fieldArray[3].innerHTML === 'o' && fieldArray[3].innerHTML === fieldArray[4].innerHTML && fieldArray[3].innerHTML === fieldArray[5].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[3], fieldArray[4], fieldArray[5])
+		hasWinner = true
+	} else if(fieldArray[6].innerHTML !== ' ' && fieldArray[6].innerHTML === 'o' && fieldArray[6].innerHTML === fieldArray[7].innerHTML && fieldArray[6].innerHTML === fieldArray[8].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[6], fieldArray[7], fieldArray[8])
+		hasWinner = true
+	} else if(fieldArray[0].innerHTML !== ' ' && fieldArray[0].innerHTML === 'o' && fieldArray[0].innerHTML === fieldArray[3].innerHTML && fieldArray[0].innerHTML === fieldArray[6].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[0], fieldArray[3], fieldArray[6])
+		hasWinner = true
+	} else if(fieldArray[1].innerHTML !== ' ' && fieldArray[1].innerHTML === 'o' && fieldArray[1].innerHTML === fieldArray[4].innerHTML && fieldArray[1].innerHTML === fieldArray[7].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[1], fieldArray[4], fieldArray[7])
+		hasWinner = true
+	} else if(fieldArray[2].innerHTML !== ' ' && fieldArray[2].innerHTML === 'o' && fieldArray[2].innerHTML === fieldArray[5].innerHTML && fieldArray[2].innerHTML === fieldArray[8].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[2], fieldArray[5], fieldArray[8])
+		hasWinner = true
+	} else if(fieldArray[0].innerHTML !== ' ' && fieldArray[0].innerHTML === 'o' && fieldArray[0].innerHTML === fieldArray[4].innerHTML && fieldArray[0].innerHTML === fieldArray[8].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[0], fieldArray[4], fieldArray[8])
+		hasWinner = true
+	} else if(fieldArray[2].innerHTML !== ' ' && fieldArray[2].innerHTML === 'o' && fieldArray[2].innerHTML === fieldArray[4].innerHTML && fieldArray[2].innerHTML === fieldArray[6].innerHTML) {
+		document.getElementById('result').innerHTML = 'O win!'
+		colorOfWinner(fieldArray[2], fieldArray[4], fieldArray[6])
+		hasWinner = true
+	} else if(fieldArray[0].innerHTML !== ' ' && fieldArray[1].innerHTML !== ' ' && fieldArray[2].innerHTML !== ' ' && fieldArray[3].innerHTML !== ' ' && fieldArray[4].innerHTML !== ' ' && fieldArray[5].innerHTML !== ' ' && fieldArray[6].innerHTML !== ' ' && fieldArray[7].innerHTML !== ' ' && fieldArray[8].innerHTML !== ' ' && hasWinner === false) {
+		document.getElementById('result').innerHTML = 'Tie'
+	}
+}
+
+function colorOfWinner(a1, a2, a3) {
+	a1.style.backgroundColor = 'red'
+	a2.style.backgroundColor = 'red'
+	a3.style.backgroundColor = 'red'
+}
+
+
+
+
+
+
+
+
+
+
+
+
